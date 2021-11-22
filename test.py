c7b3f9eab1dd275e5ed3b97f6d2ec478a0fe1da9
Khale <lkha.5023@gmail.com>
2021-11-23 00:10:10 +0700

    test recommender system

--- test.py
+++ test.py
@@ -1,19 +1,44 @@
-# import wikipedia
-# sentence = 'lý thuyết trò chơi'
-#
-# wikipedia.set_lang('vi')
-# rs = wikipedia.summary(sentence)
-# print(rs)
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 
-# test github copilot
-# get the smallest number in the list
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-def get_smallest_number(list):
-    smallest = list[0]
-    for i in list:
-        if i < smallest:
-            smallest = i
-    print(smallest)
+movieData = pd.read_csv('Data/movies_metadata.csv', low_memory=False)
+movieData = movieData.iloc[0:10000, :]
 
-get_smallest_number(list)
+#Define a TF-IDF Vectorizer Object. Remove all english stop words such as 'the', 'a'
+tfidf = TfidfVectorizer(stop_words='english')
 
+#Replace NaN with an empty string
+movieData['overview'] = movieData['overview'].fillna('')
+
+#Construct the required TF-IDF matrix by fitting and transforming the data
+tfidf_matrix = tfidf.fit_transform(movieData['overview'])
+
+# define a TF-IDF Vectorizer Object. Remove all english stop words such as 'the', 'a'
+tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+
+# compute the consine similarity matrix
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+indices = pd.Series(movieData.index, index=movieData['title']).drop_duplicates()
+
+def get_recommendations(title, cosine_sim=cosine_sim):
+    # Get the index of the movie that matches the title
+    idx = indices[title]
+
+    # Get the pairwise similarity scores of all movies with that movie
+    sim_scores = list(enumerate(cosine_sim[idx]))
+
+    # Sort the movies based on the similarity scores
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    # Get the scores of the 10 most similar movies
+    sim_scores = sim_scores[1:11]
+
+    # Get the movie indices
+    movie_indices = [i[0] for i in sim_scores]
+
+    # Return the top 10 most similar movies
+    print(movieData['title'].iloc[movie_indices])
+
+get_recommendations('Toy Story')
