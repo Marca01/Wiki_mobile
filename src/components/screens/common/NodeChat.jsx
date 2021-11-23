487d96fbdbba93befb7a938c62ab8587c7cb0086
Khale <lkha.5023@gmail.com>
2021-11-24 00:08:46 +0700

    Fix link format

--- src/components/screens/common/NodeChat.jsx
+++ src/components/screens/common/NodeChat.jsx
@@ -3,6 +3,19 @@ import { View, Text, Linking, FlatList } from "react-native";
 import { globalStyles } from "../../../styles/global";
 
 export default function NodeChat({ user_message, bot_message, data, tag }) {
+  const URL = (str) => {
+    let pattern = new RegExp(
+      "^(https?:\\/\\/)?" + // protocol
+        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+        "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+        "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+        "(\\#[-a-z\\d_]*)?$",
+      "i"
+    ); // fragment locator
+    return pattern.test(str);
+  };
+
   return (
     <View>
       {
@@ -18,15 +31,19 @@ export default function NodeChat({ user_message, bot_message, data, tag }) {
         <View style={globalStyles.messages_bot}>
           <View style={globalStyles.messages_botStyle}>
             {Array.isArray(data[1]?.message) ? (
-              data[1]?.message.map((url, i) => (
-                <Text
-                  key={i}
-                  style={{ color: "blue" }}
-                  onPress={() => Linking.openURL(url)}
-                >
-                  {url}
-                </Text>
-              ))
+              data[1]?.message.map((url, i) =>
+                URL(url) ? (
+                  <Text
+                    key={i}
+                    style={{ color: "blue" }}
+                    onPress={() => Linking.openURL(url)}
+                  >
+                    {url}
+                  </Text>
+                ) : (
+                  <Text style={globalStyles.messages_botText}>{url}</Text>
+                )
+              )
             ) : (
               <Text style={globalStyles.messages_botText}>
                 {data[1]?.message}
