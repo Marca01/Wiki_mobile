d4ff8d565edf8288d3cfce66e9a4c02b7fff8dde
Mavis2103 <taquanvku@gmail.com>
2021-11-25 19:21:54 +0700

    clipboard

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { View, Text } from "react-native";
 import axios from "axios";
+import { LOCAL } from "@env";
+const baseUrl = LOCAL;
 
-const baseUrl = "http://192.168.1.12:5000/chat";
+console.log("http", `http://${LOCAL}/chat`);
 
-export const sendUserMessage = (user_message) =>
-  axios.post("http://192.168.1.12:5000/chat", user_message, {
-    headers: { "Content-Type": "multipart/form-data" },
+export const sendUserMessage = user_message =>
+  axios.post(`http://${LOCAL}/chat`, user_message, {
+    headers: { "Content-Type": "multipart/form-data" }
   });
 
-export const getBotMessage = () =>
-  axios.get("http://192.168.1.12:5000/botMessages");
+export const getBotMessage = () => axios.get(`http://${LOCAL}/botMessages`);
