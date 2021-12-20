72ca92b5b6e37d259c847014d22d92f69c010bb1
Mavis2103 <taquanvku@gmail.com>
2021-12-20 12:04:53 +0700

    completed - touch expandable guide

--- src/styles/global.js
+++ src/styles/global.js
@@ -7,153 +7,150 @@ let width = Dimensions.get("window").width;
 let statusBarHeight = StatusBar.currentHeight;
 
 export const globalStyles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#F1F2F6",
-        paddingHorizontal: 15,
-        marginTop: statusBarHeight
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#F1F2F6",
+    paddingHorizontal: 15,
+    marginTop: statusBarHeight
+  },
 
-    // =================================================================
-    // Main.jsx
+  // =================================================================
+  // Main.jsx
 
-    messages: {
-        flex: 1,
-        // backgroundColor: 'grey',
-        flexDirection: "row"
-    },
-    messages_user: {
-        alignSelf: "flex-end",
-        marginLeft: 100
-    },
-    messages_userStyle: {
-        backgroundColor: "#4173FF",
-        borderRadius: 20,
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        marginVertical: 3
-    },
-    messages_userText: {
-        color: "white"
-    },
-    messages_bot: {
-        alignSelf: "flex-start",
-        marginTop: 10,
-        marginRight: 100
-    },
-    messages_botStyle: {
-        backgroundColor: "#fff",
-        borderRadius: 20,
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        marginVertical: 3
-    },
-    messages_botFilmText: {
-        // backgroundColor: "aqua",
-        // marginBottom: 10,
-        // paddingBottom: 10,
-        borderBottomWidth: 2,
-        borderBottomColor: "#d0d0d0",
-        flexDirection: "row",
-        alignItems: "center"
-    },
-    messages_botFilmPoster: {
-        width: 60,
-        height: 60,
-        borderRadius: 10,
-        marginRight: 8
-    },
-    messages_botText: {
-        color: "black"
-    },
-    textInput: {
-        marginBottom: 20,
-        flexDirection: "row",
-        justifyContent: "center"
-            // paddingHorizontal: 20
-    },
-    textInputMessageContainer: {
-        backgroundColor: "white",
-        borderRadius: 50,
-        flexDirection: "row",
-        alignItems: "center",
-        paddingVertical: 10,
-        paddingHorizontal: 15,
-        marginTop: 6,
-        width: width - 30
-    },
-    textInput_message: {
-        // height: 40,
-        marginLeft: 5,
-        flex: 1,
-        // paddingVertical: 10,
-        fontSize: 16,
-        fontWeight: "600"
-    },
+  messages: {
+    flex: 1,
+    // backgroundColor: 'grey',
+    flexDirection: "row"
+  },
+  messages_user: {
+    alignSelf: "flex-end",
+    marginLeft: 100
+  },
+  messages_userStyle: {
+    backgroundColor: "#4173FF",
+    borderRadius: 20,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    marginVertical: 3
+  },
+  messages_userText: {
+    color: "white"
+  },
+  messages_bot: {
+    alignSelf: "flex-start",
+    marginTop: 10,
+    marginRight: 100
+  },
+  messages_botStyle: {
+    backgroundColor: "#fff",
+    borderRadius: 20,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    marginVertical: 3
+  },
+  messages_botFilmText: {
+    // backgroundColor: "aqua",
+    // marginBottom: 10,
+    // paddingBottom: 10,
+    borderBottomWidth: 2,
+    borderBottomColor: "#d0d0d0",
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  messages_botFilmPoster: {
+    width: 60,
+    height: 60,
+    borderRadius: 10,
+    marginRight: 8
+  },
+  messages_botText: {
+    color: "black"
+  },
+  textInput: {
+    marginBottom: 20,
+    flexDirection: "row",
+    justifyContent: "center"
+    // paddingHorizontal: 20
+  },
+  textInputMessageContainer: {
+    backgroundColor: "white",
+    borderRadius: 50,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    marginTop: 6,
+    width: width - 30
+  },
+  textInput_message: {
+    // height: 40,
+    marginLeft: 5,
+    flex: 1,
+    // paddingVertical: 10,
+    fontSize: 16,
+    fontWeight: "600"
+  },
 
-    // =================================================================
-    // Header.jsx
-    header: {
-        // backgroundColor: 'red'
-    },
-    headerDiv: {
-        paddingVertical: 15,
-        marginTop: 10,
-        paddingHorizontal: 10,
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between"
-    },
-    headerLeft: {
-        flexDirection: "row",
-        alignItems: "center"
-    },
-    header_backIcon: {
-        marginLeft: -10,
-        marginRight: 8
-    },
-    headerTitle: {
-        flexDirection: "row"
-    },
-    headerTitle_title: {
-        fontSize: 24,
-        fontWeight: "bold"
-    },
-    headerRight: {},
-    header_questionIcon: {
-        padding: 2
-    },
+  // =================================================================
+  // Header.jsx
+  header: {
+    // backgroundColor: 'red'
+  },
+  headerDiv: {
+    paddingVertical: 15,
+    marginTop: 10,
+    paddingHorizontal: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  headerLeft: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  header_backIcon: {
+    marginLeft: -10,
+    marginRight: 8
+  },
+  headerTitle: {
+    flexDirection: "row"
+  },
+  headerTitle_title: {
+    fontSize: 24,
+    fontWeight: "bold"
+  },
+  headerRight: {},
+  header_questionIcon: {
+    padding: 2
+  },
 
-    // =================================================================
-    // Guide.jsx
-    guideHeader: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-    },
-    guideHeader_title: {
-
-    },
-    guideFeature: {
-
-    },
-    guideFeature_title: {
-        fontSize: 32,
-        fontWeight: "bold"
-    },
-    guideFeature_body: {
-
-    },
-    guideFeature_syntax: {
-
-    },
-    guideFeature_description: {
-
-    },
-    // test style
-    testStyle: {
-        backgroundColor: "gray",
-        flex: 1,
-        height: height
-    }
+  // =================================================================
+  // Guide.jsx
+  guideHeader: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "space-between"
+  },
+  guideHeader_title: {
+    marginVertical: 25
+  },
+  guideFeature: {},
+  guideFeature_title: {
+    fontSize: 18,
+    marginTop: 12,
+    marginBottom: 8,
+    fontWeight: "bold",
+    color: "#141414"
+  },
+  guideFeature_body: {
+    color: "#404040"
+  },
+  guideFeature_syntax: {},
+  guideFeature_description: {},
+  // test style
+  testStyle: {
+    backgroundColor: "gray",
+    flex: 1,
+    height: height
+  }
 });
